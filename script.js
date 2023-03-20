6e5012fd66b497f1bf11079b611b04a899be35d8
Giovanna Oliveira <107190603+ansiee@users.noreply.github.com>
2023-03-20 10:03:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const regex = /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,16}$/;
+const regexSenha =
+  /^(?!.*\s)(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{10,16}$/;
+const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-const loginBtn = document.getElementById('loginBtn');
-const senhaInput = document.getElementById('senha');
+const senha = document.getElementById("senha");
+const email = document.getElementById("email");
+const msg = document.getElementById("status");
 
-loginBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    if(regex.test(senhaInput.value)){
-        alert("Você está logado!")
-    } else {
-        senhaInput.style.border = "2px solid red";
+document.getElementById("loginBtn").addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("teste");
+  if (regexSenha.test(senha.value) && regexEmail.test(email.value)) {
+    console.log("pop");
+    msg.style.color = "green";
+    msg.style.display = "block";
+    msg.innerHTML = "Seu login foi concluído com sucesso!!";
+  } else {
+    if (regexSenha.test(senha.value) == false) {
+      msg.style.color = "red";
+      msg.style.display = "block";
+      msg.innerHTML = "Sua senha é inválida, tente reescrever.";
     }
+    if (regexEmail.test(email.value) == false) {
+      msg.style.color = "red";
+      msg.style.display = "block";
+      msg.innerHTML = "Seu e-mail é inválido, tente reescrever.";
+    }
+  }
 });
